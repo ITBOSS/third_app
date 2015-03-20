cb6e5150d3aba837251d35ee460f6fc5eba48d5f
ITBOSS <git@itbose.com>
2015-03-20 17:41:10 +0900

    add board#create

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,7 +1,20 @@
 class BoardsController < ApplicationController
   def index
   end
-  
+
   def new
+    @board = Board.new
+  end
+
+  def create
+    @board = Board.new(params_board)
+    @board.save
+    redirect_to "/boards"
+  end
+
+  private
+
+  def params_board
+    params.permit(:title, :editor)
   end
 end
